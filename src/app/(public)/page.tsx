27d2e77d00a140bcd6e3741cf508779685832777
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-08-06 21:38:19 +0200

    Refactor HomePage component to include CommandPalette and improve structure

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,10 +1,26 @@
-import Hero from './components/HeroSection';
+import CommandPalette from '@/components/CommandPalette';
 
-export default function Home() {
+import ContactSection from './components/ContactSection';
+import Education from './components/Education';
+import HeroSection from './components/HeroSection';
+import ProfessionalSummary from './components/ProfessionalSummary';
+import Projects from './components/Projects';
+import TechnicalSkills from './components/TechnicalSkills';
+import WorkExperience from './components/WorkExperience';
+
+export default function HomePage() {
   return (
-    // This is the main page of the application
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Hero />
-    </main>
+    <>
+      <CommandPalette />
+      <main className='min-h-screen'>
+        <HeroSection />
+        <ProfessionalSummary />
+        <TechnicalSkills />
+        <WorkExperience />
+        <Projects />
+        <Education />
+        <ContactSection />
+      </main>
+    </>
   );
 }
