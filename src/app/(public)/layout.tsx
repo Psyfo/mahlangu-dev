e41c592033cc3d281f1399d39d1573746ffa1800
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-08-07 17:07:05 +0200

    Add canonical link to RootLayout for improved SEO

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -58,6 +58,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
+        <link rel='canonical' href='https://mahlangu.dev' />
         <script
           type='application/ld+json'
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
